30691d2d7073e3b375082e48ee59e422f1ea3614
hardeepsysbit <hardeep@sysbit.com>
2021-08-04 02:01:48 +0800

    2021080402:01

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,9 +5,9 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { connect } from 'react-redux';
 
 import './header.styles.scss';
-
-
-const Header = ({currentUser}) => (
+import  CartIcon  from '../cart-icon/cart-icon.component'
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
+const Header = ({currentUser, hidden}) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -25,14 +25,20 @@ const Header = ({currentUser}) => (
         :
         <Link className='option' to='/signin'>SIGN IN</Link> 
       }
-    </div>
+      <CartIcon/>
+      {hidden ? null :  <CartDropDown/>}
+         </div>
   </div>
 );
 
-const mapStateToProps = state => ({
-     currentUser: state.user.currentUser 
-})
+// const mapStateToProps = state => ({
+//      currentUser: state.user.currentUser 
+// })
 
-
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+       currentUser,
+       hidden 
+   })
+  
 
 export default connect(mapStateToProps)(Header)
